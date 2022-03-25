3db3d390d8b9b97ad421049e166d1dd13d1f3f2e
xiefeicomeon <1300295507@qq.com>
2022-03-25 12:40:50 +0800

    修正pandas读取excel和csv文件将整型数据自动转换成浮点数bug

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ def generateCSV(Originsheet_path,  Output_path):
     Dir_name = Output_path / '主站CSV' / Station.group()
     Path(Dir_name).mkdir(parents=True, exist_ok=True)
     # 读取站端遥信表
-    Origin_sheet_yx = pd.read_excel(Originsheet_path, "遥信")
+    Origin_sheet_yx = pd.read_excel(Originsheet_path, "遥信",dtype=object)
 
     # 保存主站遥信表
     YX_path = Dir_name / 'csv_dig.csv'
     Origin_sheet_yx.to_csv(YX_path, encoding="gbk", index=None)
 
     # 读取主站遥测表
-    Origin_sheet_yc = pd.read_excel(Originsheet_path, "遥测")
+    Origin_sheet_yc = pd.read_excel(Originsheet_path, "遥测",dtype=object)
 
     # 保存主站遥测表
     YC_path = Dir_name / 'csv_ana.csv'
     Origin_sheet_yc.to_csv(YC_path, encoding="gbk", index=None)
 
     # 读取主站遥控表
-    Origin_sheet_yk = pd.read_excel(Originsheet_path, "遥控")
-
-    # 保存主站遥控表
+    Origin_sheet_yk = pd.read_excel(Originsheet_path, "遥控", dtype=object)
+    # print(Origin_sheet_yk)
+    # 保存主站遥控
     YK_path = Dir_name / 'csv_digctl.csv'
     Origin_sheet_yk.to_csv(YK_path, encoding="gbk", index=None)
 
@@ -39,7 +39,7 @@ def generateCSV(Originsheet_path,  Output_path):
 
     # 保存主站遥调表
     YT_path = Dir_name / 'csv_anactl.csv'
-    Origin_sheet_yt.to_csv(YT_path, encoding="gbk", index=None)
+    Origin_sheet_yt.to_csv(YT_path, encoding="gbk", index=None,float_format=str)
     print('%s生成CSV文件成功' %Station.group())
 
 # 当前项目路径
